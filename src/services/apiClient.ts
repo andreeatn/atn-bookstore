ce4466fe3b89bc021b4ed0491c775c9d73cf119d
Andreea TN <andreeatudn@gmail.com>
2023-11-09 19:16:52 +0200

    Add Env Key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export default axios.create({
     baseURL: 'https://www.googleapis.com/books/v1/',
     params:{
-        key: 'AIzaSyB0HQy1eR2FRQMebqLzF95rCnQfdrA5bHs'
+        key: API_KEY
     }
 })
 

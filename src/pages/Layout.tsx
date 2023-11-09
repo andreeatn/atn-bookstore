e675dd12009bd91ff2d7d969dd97fdd78bb64cf7
Andreea TN <andreeatudn@gmail.com>
2023-11-09 13:52:11 +0200

    Modify FooterSubscribeForm: add functionality to form subscribe event

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
-import Footer from "components/Footer";
 import { VolumeQueryProvider } from "context/VolumeQueryContext";
 import ThemeContext from "context/ThemeContext";
 import { useContext } from "react";
 import { CartProvider } from "context/CartContext";
+import Footer from "components/Footer/Footer";
 
 function Layout() {
   const { baseColor } = useContext(ThemeContext);
